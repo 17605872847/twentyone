adb2bd7fd39c6b36a4436e80a97f484a3c23cf2d
lp726 <lp726@qq.com>
2019-08-01 10:49:50 +0800

    new

--- 打地鼠/src/play.js
+++ 打地鼠/src/play.js
@@ -1,7 +1,8 @@
-var timer=null;
+var timer = null;
 $('#play').click(function () {
     var time = 30;
-    $('#fenshu').html('0')
+    var fenshu = 0;
+    $('#fenshu').html(fenshu)
     $('#play p').hide();
     $('#time').html(time);
     var timer = setInterval(function () {
@@ -18,31 +19,33 @@ $('#play').click(function () {
     var playgame = null;
     var lala = null;
     clearInterval(playgame);
-     playgame = setInterval(function () {
+    playgame = setInterval(function () {
         var index = Math.floor((Math.random() * arr.length));
         console.log(index)
         $(arr[index]).animate({ 'top': '80px' }, 300)
-        lala = setInterval(function () {
-            $(arr[index]).animate({ 'top': '200px' }, 300)
-        }, 700)
-        // lala = setTimeout(function () {
+        // lala = setInterval(function () {
         //     $(arr[index]).animate({ 'top': '200px' }, 300)
-        // }, 1350)
+        // }, 700)
+        lala = setTimeout(function () {
+            $(arr[index]).animate({ 'top': '200px' }, 300)
+        }, 450)
         if (time <= 1) {
             clearInterval(playgame);
         }
-    }, 1200)
+    }, 1000)
+
+    var fenshu = 0;
+    $('.game div img').click(function () {
+        event.preventDefault();
+        $('.game div img').animate({ 'top': '200px' }, 50)
+        fenshu += 1;
+        $('#fenshu').html(fenshu);
+    })
+
+    $('img').on('mousedown', function (e) {
+        e.preventDefault()
+    })
 })
 
-var fenshu = 0;
-$('.game div img').click(function(){
-    event.preventDefault();
-    $('.game div img').animate({ 'top': '200px' }, 50)
-    fenshu += 1;
-    $('#fenshu').html(fenshu);
-})
 
-$('img').on('mousedown',function (e) {
-    e.preventDefault()
-})
 //console.log(arr.sort(()=>{ return 0.5 - Math.random()}));
